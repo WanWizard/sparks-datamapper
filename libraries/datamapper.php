082cbce7766499d1812e9ba1597ce5ca85f7a1c7
WanWizard <wanwizard@wanwizard.eu>
2011-12-30 19:44:08 +0100

    bumped the version to 1.8.2.1.

--- libraries/datamapper.php
+++ libraries/datamapper.php
@@ -12,7 +12,7 @@
  * @author  	Phil DeJarnett (up to v1.7.1)
  * @author  	Simon Stenhouse (up to v1.6.0)
  * @link		http://datamapper.wanwizard.eu/
- * @version 	1.8.2
+ * @version 	1.8.2.1
  */
 
 /**
@@ -23,7 +23,7 @@ define('DMZ_CLASSNAMES_KEY', '_dmz_classnames');
 /**
  * DMZ version
  */
-define('DMZ_VERSION', '1.8.2');
+define('DMZ_VERSION', '1.8.2.1');
 
 /**
  * Data Mapper Class
